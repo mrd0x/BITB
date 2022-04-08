e79fcada9cfff4abd05811fc08cec5052d4a070b
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-04-08 09:09:49 +0100

    ignore

---
+++ app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var useragent = require('useragent');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.use('/', (req, res, next) => {
+
+  ua = useragent.parse(req.headers['user-agent'])// true
+  console.log(ua.os)
+  if (ua.os.family==='Windows')
+  {
+    console.log(ua.os)
+    app.set('views', path.join(__dirname, 'views/Windows-DarkMode-Delay'));
+  }
+  else {
+    app.set('views', path.join(__dirname, 'views/Windows-DarkMode-Delay'));
+    
+  }
+  
+    next();
+  });
+
+
+
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public/Windows-DarkMode-Delay')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('..\\error');
+});
+
+module.exports = app;
---
+++ public/Windows-Chrome-LightMode/script.js
@@ -0,0 +1,92 @@
+var minimize = document.getElementById("minimize");
+var square = document.getElementById("square");
+var exit = document.getElementById("exit");
+var titleBar = document.getElementById("title-bar");
+
+////////////////// Hover listeners //////////////////
+minimize.addEventListener('mouseover', function handleMouseOver() {
+  minimize.style.backgroundColor = 'rgba(0, 0, 0, 0.09765625)';
+  minimize.style.cursor = 'context-menu';
+});
+
+minimize.addEventListener('mouseout', function handleMouseOut() {
+  minimize.style.backgroundColor = '#d6dae0';
+  minimize.style.cursor = 'default';
+});
+
+square.addEventListener('mouseover', function handleMouseOver() {
+  square.style.backgroundColor = 'rgba(0, 0, 0, 0.09765625)';
+  square.style.cursor = 'context-menu';
+});
+
+square.addEventListener('mouseout', function handleMouseOut() {
+  square.style.backgroundColor = '#d6dae0';
+  square.style.cursor = 'default';
+});
+
+exit.addEventListener('mouseover', function handleMouseOver() {
+  exit.style.backgroundColor = '#E81123';
+  exit.style.cursor = 'context-menu';
+});
+
+exit.addEventListener('mouseout', function handleMouseOut() {
+  exit.style.backgroundColor = '#d6dae0';
+  exit.style.cursor = 'default';
+});
+
+titleBar.addEventListener('mouseover', function handleMouseOver() {
+  titleBar.style.cursor = 'context-menu';
+});
+
+titleBar.addEventListener('mouseout', function handleMouseOver() {
+  titleBar.style.cursor = 'default';
+});
+
+
+//////////////// Make window draggable start ////////////////
+// Make the DIV element draggable:
+var draggable = $('#window');
+var title = $('#title-bar');
+
+title.on('mousedown', function(e){
+	var dr = $(draggable).addClass("drag");
+	height = dr.outerHeight();
+	width = dr.outerWidth();
+	ypos = dr.offset().top + height - e.pageY,
+	xpos = dr.offset().left + width - e.pageX;
+	$(document.body).on('mousemove', function(e){
+		var itop = e.pageY + ypos - height;
+		var ileft = e.pageX + xpos - width;
+		if(dr.hasClass("drag")){
+			dr.offset({top: itop,left: ileft});
+		}
+	}).on('mouseup', function(e){
+			dr.removeClass("drag");
+	});
+});
+//////////////// Make window draggable end ////////////////
+
+
+////////////////// Onclick listeners //////////////////
+// X button functionality
+$("#exit").click(function(){
+    $("#window").css("display", "none");
+  });
+
+// Maximize button functionality
+$("#square").click(enlarge);
+
+function enlarge(){
+  if(square.classList.contains("enlarged")){
+    $("#window").css("width", "40%");
+    $("#title-bar-width").css('width', '100%');
+    $("#content").css("width", "100%");
+    $("#square").removeClass("enlarged");
+  }
+  else{
+    $("#window").css("width", "70%");
+    $("#title-bar-width").css('width', '100%');
+    $("#content").css("width", "100%");
+    $("#square").addClass("enlarged");
+  }
+}
